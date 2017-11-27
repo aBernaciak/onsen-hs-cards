397d3c02fd921f45def4e8ed15e7822cc3eb62e9
Sintari <aleks.bernaciak@gmail.com>
2017-11-27 16:01:00 +0100

    added funcionality to check cards

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -14,20 +14,34 @@
     </v-ons-toolbar>
 
     <div class="header">
-      <img src="https://eu.battle.net/forums/static/images/game-logos/game-logo-hearthstone.png">
+      <img src="https://eu.battle.net/forums/static/images/game-logos/game-logo-hearthstone.png" class="hs-logo">
     </div>
-     <v-ons-list v-if="ifCardChosen">
+    <v-ons-list>
       <v-ons-list-header>
         Card
       </v-ons-list-header>
       <v-ons-list-item>
-        <div class="center">{{cardChosen[0].name}}</div>
-        <img :src="cardChosen[0].src" alt="" width="256" height="382">
+        <div class="card-container" :class="{flipped : ifCardChosen}">
+          <img src="../assets/cardback_0.png" alt="" class="card-initial">
+          <img :src="cardChosen[0].src" alt="" class="card-flipped" v-if="ifCardChosen">
+          <img src="../assets/cardback_0.png" class="card-flipped "alt="" v-else>
+        </div>
       </v-ons-list-item>
-    </v-ons-list>
-    <v-ons-list v-else>
-      <v-ons-list-item>
-        <img src="http://media-hearth.cursecdn.com/attachments/39/664/cardback_0.png" alt="">
+      <v-ons-list-item v-if="ifCardChosen">
+        {{cardChosen[0].name}} art by {{cardChosen[0].artist}}
+        <pre>{{cardChosen[0].flavor}}</pre>
+        <br>
+        <div class="card-extra-info">
+          <v-ons-icon style="color: green;" icon="fa-check" v-if="cardChosen[0].collectible"></v-ons-icon>
+          <v-ons-icon style="color: red;" icon="fa-times" v-else></v-ons-icon>
+          Collectable
+          <br>
+          <v-ons-icon style="color: green;" icon="fa-unlock" v-if="cardChosen[0].howToEarn"></v-ons-icon>
+          {{cardChosen[0].howToEarn}}
+          <br>
+          <v-ons-icon style="color: gold;" icon="fa-unlock" v-if="cardChosen[0].howToEarnGolden"></v-ons-icon>
+          {{cardChosen[0].howToEarnGolden}}
+        </div>
       </v-ons-list-item>
     </v-ons-list>
     <v-ons-list>
@@ -36,7 +50,7 @@
       </v-ons-list-header>
       <v-ons-list-item>
         <div class="center">
-          <v-ons-input placeholder="Search for card by name" float v-model="name">
+          <v-ons-input placeholder="Search for card by name" float v-model="name" @keyup.enter="search(name)">
           </v-ons-input>
         </div>
       </v-ons-list-item>
@@ -72,28 +86,31 @@ export default {
     }
   },
   methods: {
+    toTitleCase(str) {
+      return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    },
     search(name) {
-      console.log(name)
+      this.ifCardChosen = false;
+      name = this.toTitleCase(name);
       var array = this.cardArray;
       if (name != '') {
         this.cardChosen = array.filter(function (el) {
           if (el.name == name) {
             return el.name == name;
-            this.cardChosen[0].src = `https://art.hearthstonejson.com/v1/render/latest/${this.langChosen}/256x/${this.cardChosen[0].id}.png`;
-            this.ifCardChosen = true;
           }
         });
         if (this.cardChosen.length == 0) {
           this.$ons.notification.toast('We couldn\'t find that card.', {timeout: 2000});
+        }
+        else {
+          this.cardChosen[0].src = `https://art.hearthstonejson.com/v1/render/latest/${this.langChosen}/256x/${this.cardChosen[0].id}.png`;
+          this.ifCardChosen = true;
         }
       }
       else {
         this.$ons.notification.toast('Input cannot be empty.', {timeout: 2000});
       }
       console.log(this.cardChosen[0])
-    },
-    getData(obj){
-      console.log(obj);
     },
     changeLanguage(lang) {
       this.langChosen = lang;
@@ -115,14 +132,51 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 .header {
   text-align: center;
 }
 
-img {
+.card-extra-info {
+  display: block;
+  width: 100%;
+}
+
+.card-container {
+  margin: 0 auto;
+  transform: rotateY(0);
+  transform-style: preserve-3d;
+  transition: all .3s linear;
+  width: 256px;
+  height: 382px;
+  position: relative;
+  margin: -30px auto -30px;
+  &.flipped {
+    transform: rotateY(180deg);
+    .card-flipped {
+      // opacity: 1;
+    }
+  }
+  img {
+    &.card-initial {
+      position: absolute;
+      backface-visibility: hidden;
+    }
+    &.card-flipped {
+      transition: opacity 0.5s linear;
+      transform: rotateY(180deg);
+      transform: rotate3d(0, 1, 0, 180deg);
+      // opacity: 0;
+    }
+  }
+}
+.hs-logo {
   max-width: 300px;
   margin: 0 auto;
+}
+
+pre {
+  white-space: normal;
 }
 
 ons-list-title:not(:first-of-type) {
